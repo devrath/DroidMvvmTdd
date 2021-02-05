c5a9355498fa182a1d0c383a3348a2bb77294f94
Devrath AD <devrathad@mplgaming.com>
2021-02-05 21:02:19 +0530

    added dependencies

--- app/src/main/java/com/demo/code/application/PlaylistApplication.kt
+++ app/src/main/java/com/demo/code/application/PlaylistApplication.kt
@@ -1,9 +1,23 @@
 package com.demo.code.application
 
 import android.app.Application
+import com.demo.code.di.*
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 @HiltAndroidApp
 class PlaylistApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@PlaylistApplication)
+            modules(listOf(repositoryModule, viewModelModule, retrofitModule, apiModule, serviceModule))
+        }
+    }
 
 }
